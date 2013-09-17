3ca8f8d893cdcb3fb86a09dd527ffcd1b0d6a02c
ngnono <ngnono@126.com>
2013-09-17 18:14:46 +0800

    edit pagerinfo

--- NGnono.Framework/NGnono.Framework/Models/PagerInfo.cs
+++ NGnono.Framework/NGnono.Framework/Models/PagerInfo.cs
@@ -5,41 +5,43 @@ using System.Runtime.Serialization;
 namespace NGnono.Framework.Models
 {
     /// <summary>
-    /// 可分页的
+    ///     可分页的
     /// </summary>
-    public interface IPagedable
+    public interface IPagerable
     {
         /// <summary>
-        /// 页码(从1起始)
+        ///     页码(从1起始)
         /// </summary>
-        int Index { get; set; }
+        int Index { get; }
 
         /// <summary>
-        /// 页大小
+        ///     页大小
         /// </summary>
         int Size { get; }
 
         /// <summary>
-        /// 总页数
+        ///     总页数
         /// </summary>
         int TotalPaged { get; }
 
         /// <summary>
-        /// 总记录数
+        ///     总记录数
         /// </summary>
         int TotalCount { get; }
     }
 
     /// <summary>
-    /// 可分页迭代器
+    ///     可分页迭代器
     /// </summary>
     /// <typeparam name="T">类型</typeparam>
-    public interface IPagedEnumerable<out T> : IEnumerable<T>, IPagedable
+    public interface IPagerEnumerable<out T> : IEnumerable<T>, IPagerable
     {
     }
 
-    //[DataContract(Name = "pager")]
-    public class PagerInfo<T> : List<T>, IPagedEnumerable<T>
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagerInfo<T> : List<T>, IPagerEnumerable<T>
     {
         #region fields
 
@@ -47,20 +49,29 @@ namespace NGnono.Framework.Models
 
         #region .ctor
 
+        /// <summary>
+        /// </summary>
         public PagerInfo()
         {
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="request"></param>
         public PagerInfo(PagerRequest request)
             : this(request, 0)
         {
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="totalCount"></param>
         public PagerInfo(PagerRequest request, int totalCount)
         {
-            this.Index = request.PageIndex;
-            this.Size = request.PageSize;
-            this.TotalCount = totalCount;
+            Index = request.PageIndex;
+            Size = request.PageSize;
+            TotalCount = totalCount;
         }
 
         #endregion
@@ -68,47 +79,39 @@ namespace NGnono.Framework.Models
         #region properties
 
         /// <summary>
-        /// 索引
+        ///     是否存在分页
+        /// </summary>
+        [DataMember(Name = "ispaged", Order = 5)]
+        public bool IsPaged
+        {
+            get { return Size < TotalCount; }
+        }
+
+        /// <summary>
+        ///     索引
         /// </summary>
         [DataMember(Name = "pageindex", Order = 1)]
         public int Index { get; set; }
 
         /// <summary>
-        /// 大小
+        ///     大小
         /// </summary>
         [DataMember(Name = "pagesize", Order = 2)]
         public int Size { get; set; }
 
         /// <summary>
-        /// 总数据量
+        ///     总数据量
         /// </summary>
         [DataMember(Name = "totalcount", Order = 3)]
         public int TotalCount { get; set; }
 
         /// <summary>
-        /// 总页数
+        ///     总页数
         /// </summary>
         [DataMember(Name = "totalpaged", Order = 4)]
         public int TotalPaged
         {
-            get
-            {
-                return (int)Math.Ceiling(this.TotalCount / (double)this.Size);
-            }
-            set { }
-        }
-
-        /// <summary>
-        /// 是否存在分页
-        /// </summary>
-        [DataMember(Name = "ispaged", Order = 5)]
-        public bool IsPaged
-        {
-            get
-            {
-                return Size < TotalCount;
-            }
-            set { }
+            get { return (int)Math.Ceiling(TotalCount / (double)Size); }
         }
 
         #endregion
@@ -119,31 +122,46 @@ namespace NGnono.Framework.Models
     }
 
     /// <summary>
-    /// 分页请求
+    ///     分页请求
     /// </summary>
     [DataContract]
     public class PagerRequest
     {
         private readonly int _maxPageSize;
 
+        /// <summary>
+        ///     local pageNumber
+        /// </summary>
+        private int _pageIndex;
+
+        /// <summary>
+        ///     local pageSize
+        /// </summary>
+        private int _pageSize;
+
+        /// <summary>
+        /// </summary>
         public PagerRequest()
             : this(40)
         {
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="maxPageSize"></param>
         public PagerRequest(int maxPageSize)
         {
-            this._maxPageSize = maxPageSize;
+            _maxPageSize = maxPageSize;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PagerRequest"/> class.
+        ///     Initializes a new instance of the <see cref="PagerRequest" /> class.
         /// </summary>
         /// <param name="pageNumber">
-        /// The page number.
+        ///     The page number.
         /// </param>
         /// <param name="pageSize">
-        /// The page size.
+        ///     The page size.
         /// </param>
         public PagerRequest(int pageNumber, int pageSize)
             : this(pageNumber, pageSize, 40)
@@ -151,64 +169,42 @@ namespace NGnono.Framework.Models
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PagerRequest"/> class.
+        ///     Initializes a new instance of the <see cref="PagerRequest" /> class.
         /// </summary>
         /// <param name="pageNumber">
-        /// The page number.
+        ///     The page number.
         /// </param>
         /// <param name="pageSize">
-        /// The page size.
+        ///     The page size.
         /// </param>
         /// <param name="maxPageSize"> </param>
         public PagerRequest(int pageNumber, int pageSize, int maxPageSize)
             : this(maxPageSize)
         {
-            this.PageSize = pageSize;
-            this.PageIndex = pageNumber;
+            PageSize = pageSize;
+            PageIndex = pageNumber;
         }
 
         /// <summary>
-        /// local pageNumber
-        /// </summary>
-        private int _pageIndex;
-
-        /// <summary>
-        /// local pageSize
-        /// </summary>
-        private int _pageSize;
-
-        /// <summary>
-        /// 页大小
+        ///     页大小
         /// </summary>
         [DataMember]
         public int PageSize
         {
-            get
-            {
-                return this._pageSize;
-            }
+            get { return _pageSize; }
 
-            private set
-            {
-                this._pageSize = (value < 1 ? 1 : (value > _maxPageSize ? _maxPageSize : value));
-            }
+            private set { _pageSize = (value < 1 ? 1 : (value > _maxPageSize ? _maxPageSize : value)); }
         }
 
         /// <summary>
-        /// 页码
+        ///     页码
         /// </summary>
         [DataMember]
         public int PageIndex
         {
-            get
-            {
-                return this._pageIndex;
-            }
+            get { return _pageIndex; }
 
-            private set
-            {
-                this._pageIndex = value < 1 ? 1 : value;
-            }
+            private set { _pageIndex = value < 1 ? 1 : value; }
         }
     }
 }
